3878e32c161b9659ec98a838914bbe98852d6f72
A1909bek <aa2004bek@gmail.com>
2025-05-19 16:54:13 +0500

    add

--- config/settings.py
+++ config/settings.py
@@ -165,8 +165,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp-relay.brevo.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")  # Brevo hisobingizdagi email
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")      # API key (SMTP parol o‘rnida)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")  
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")     
 
 
 
