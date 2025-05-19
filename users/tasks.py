2fa4793e3ce4682c637f0a72bfe29c32fb796dd4
A1909bek <aa2004bek@gmail.com>
2025-05-19 21:23:09 +0500

    adding some

--- users/tasks.py
+++ users/tasks.py
@@ -1,21 +1,13 @@
 from celery import shared_task
 from django.core.mail import send_mail
-from django.conf import settings
-import logging
-
-logger = logging.getLogger(__name__)
+from config.settings import DEFAULT_FROM_EMAIL
 
 @shared_task
-def send_otp_email_task(email, otp):
-    try:
-        send_mail(
-            subject='Tizimga kirishni tasdiqlash kodi',
-            message=f"Sizning tizimga kirishni tasdiqlash kodingiz {otp}",
-            from_email=settings.DEFAULT_FROM_EMAIL,
-            recipient_list=[email],
-            fail_silently=False
-        )
-        logger.info(f"OTP {otp} successfully sent to {email}")
-    except Exception as e:
-        logger.error(f"Failed to send OTP to {email}: {str(e)}")
-        raise  
+def sent_otp_email_task(email,otp):
+    send_mail(
+        subject='Tizimga kirishni tasdiqlash kodi',
+        message=f"Sizning tizimga kirishni tasdiqlash kodingiz {otp}",
+        from_email= DEFAULT_FROM_EMAIL,
+        recipient_list=[email],
+        fail_silently=False
+    )
