1184907f2eb1813d94cb02ee2163c220e8aba945
A1909bek <aa2004bek@gmail.com>
2025-05-19 20:46:48 +0500

    adding changes

--- config/settings.py
+++ config/settings.py
@@ -161,13 +161,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='pro.turbo-smtp.com'
-EMAIL_PORT='587'
-EMAIL_USE_TLS=True
-EMAIL_HOST_USER=config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD=config("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL=config("DEFAULT_FROM_EMAIL")
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'pro.turbo-smtp.com'
+EMAIL_PORT = 587  
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False 
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")  
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 
    
--- users/tasks.py
+++ users/tasks.py
@@ -7,7 +7,7 @@ def sent_otp_email_task(email,otp):
     send_mail(
         subject='Tizimga kirishni tasdiqlash kodi',
         message=f"Sizning tizimga kirishni tasdiqlash kodingiz {otp}",
-        from_email= None,
+        from_email= EMAIL_HOST_USER,
         recipient_list=[email],
         fail_silently=False
     )
