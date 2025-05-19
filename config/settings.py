d60cfc9c67fa11b0ad4cbedb6d55c6af793aa953
A1909bek <aa2004bek@gmail.com>
2025-05-19 18:23:15 +0500

    adding some changes

--- config/settings.py
+++ config/settings.py
@@ -161,8 +161,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
-SENDER_NET_API_TOKEN = config('SENDER_NET_API_TOKEN')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+
    
 
 
