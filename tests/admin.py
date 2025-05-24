85d9dfa90ea5f60dca24e83f1f4a6c9e9664ec79
A1909bek <aa2004bek@gmail.com>
2025-05-24 14:52:55 +0500

    adding some changes

--- tests/admin.py
+++ tests/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from tests.models import Category,Test,Question
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Test)
+admin.site.register(Question)
--- tests/views.py
+++ tests/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,get_object_or_404
-
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.views import APIView
 from rest_framework import permissions,status
 from rest_framework.response import Response
@@ -13,6 +13,12 @@ from tests.models import Test,Category,Question
 class CategoryAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
+    @swagger_auto_schema(
+        request_body=CategorySerializer,
+        responses={200: CategorySerializer},
+        operation_description="Yangi test yaratish (faqat superuser)"
+    )
+
     def get(self,request):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories,many=True)
@@ -25,6 +31,12 @@ class CategoryAPIView(APIView):
     
 class CategoryCreateAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
+
+    @swagger_auto_schema(
+        request_body=CategorySerializer,
+        responses={201: CategorySerializer},
+        operation_description="Yangi test yaratish (faqat superuser)"
+    )
 
     def post(self,request):
         if not request.user.is_superuser:
@@ -48,6 +60,12 @@ class CategoryCreateAPIView(APIView):
 class CategoryDetailAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
+    @swagger_auto_schema(
+        request_body=CategoryDetailSerializer,
+        responses={200: CategoryDetailSerializer},
+        operation_description="Categoriyadagi testlar ro'yhati" 
+    )
+
     def get(self,request,category_id):
         try:
             category = Category.objects.get(id=category_id)
@@ -68,6 +86,12 @@ class CategoryDetailAPIView(APIView):
 
 class TestCreateAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
+
+    @swagger_auto_schema(
+        request_body=TestCreateSerializer,
+        responses={201: TestCreateSerializer},
+        operation_description="Yangi test yaratish (faqat superuser)"
+    )
 
     def post(self,request):
         if not request.user.is_superuser:
@@ -90,6 +114,12 @@ class TestCreateAPIView(APIView):
 class FreeTestAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
 
+    @swagger_auto_schema(
+        request_body=TestSerializer,
+        responses={200: TestSerializer},
+        operation_description="Tekin testlar ro'yhatini olish"
+    )
+
     def get(self,request):
         tests = Test.objects.filter(is_paid=False)
         serializer = TestSerializer(tests,many=True)
@@ -102,6 +132,12 @@ class FreeTestAPIView(APIView):
     
 class PaidTestAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
+
+    @swagger_auto_schema(
+        request_body=TestSerializer,
+        responses={200: TestSerializer},
+        operation_description="Pulli testlar ro'yhatini olish"
+    )
 
     def get(self,request):
         tests = Test.objects.filter(is_paid=True)
@@ -116,6 +152,12 @@ class PaidTestAPIView(APIView):
 
 class QuestionCreateAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
+
+    @swagger_auto_schema(
+        request_body=QuestionCreateSerializer,
+        responses={201: QuestionCreateSerializer},
+        operation_description="Yangi savol yaratish (faqat superuser)"
+    )
 
     def post(self, request, test_id):
         if not request.user.is_superuser:
@@ -145,6 +187,12 @@ class QuestionCreateAPIView(APIView):
         
 class TestDetailAPIView(APIView):
     permission_classes = [permissions.IsAuthenticated]
+
+    @swagger_auto_schema(
+        request_body=TestDetailSerializer,
+        responses={200: TestDetailSerializer},
+        operation_description="Testdagi savollar ro'yhati"
+    )
 
     def get(self,request,test_id):
         try:
